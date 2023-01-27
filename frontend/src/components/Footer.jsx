f65683d6cec24954afbaf302012248465321d06e
destroyer22719 <nathancai1031@gmail.com>
2023-01-27 18:28:22 -0500

    styled layout, nav, and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
+import "../../styles/components/footer.scss"
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer">
       <div>This project is made for QHacks 2023</div>
       <div>
         <a href="https://github.com/ammarlakda/scholarscribe" target="_blank">
-          <FaGithub />
+          <FaGithub className="footer__gh-icon"/>
         </a>
       </div>
     </div>
