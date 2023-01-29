685b929c8f11f63454e9541cfd54c8d88dc00fe5
RobbieHuang2002 <robbie.huang2002@gmail.com>
2023-01-28 19:52:20 -0500

    created app.route

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
+import os
+
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def helloworld():
+    return 'helloworld'
+
 
 @app.route('/', methods=['POST'])
 def handle_post_request():
     data = request.files['file']
-    print(data)
-    return "Received POST request"
+    if data:
+        print('true')
 
 if __name__ == '__main__':
     app.run(port=3000)
