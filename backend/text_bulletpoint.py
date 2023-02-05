9200d508febfab177c175910b1b4d3c86985cab7
Ammar Lakda <ammarlakdawala91@gmail.com>
2023-02-04 21:46:29 -0500

    bullet points done

--- backend/text_bulletpoint.py
+++ backend/text_bulletpoint.py
@@ -1,27 +1,26 @@
 import pyperclip
    
-  
-# saves text copied to clipboard
-# in variable text
-text = pyperclip.paste()
-print("Before Modification:")
-print(text)
-   
-# stores different lines of text
-# in a list named lines
-lines = text.split("\n")
-   
-# adds * to every line stored
-# in list
-for i in range(len(lines)):
-    lines[i] = "*" + lines[i]
-  
-# converts the list of different
-# lines to single text
-text = "\n".join(lines)
-   
-# copies new modified text
-# to the clipboard
-pyperclip.copy(text)
-print("\nAfter Modification:") 
-print(pyperclip.paste())
+
+def bullet(summarized_text): 
+    # saves text copied to clipboard
+    # in variable text
+    text = pyperclip.copy(summarized_text)
+    text = pyperclip.paste()
+    
+    # stores different lines of text
+    # in a list named lines
+    lines = text.split(".")
+    
+    # adds * to every line stored
+    # in list
+    for i in range(len(lines)):
+        lines[i] = "." + lines[i]
+    
+    # converts the list of different
+    # lines to single text
+    text = "\n".join(lines)
+    
+    # copies new modified text
+    # to the clipboard
+    pyperclip.copy(text) 
+    return pyperclip.paste()
