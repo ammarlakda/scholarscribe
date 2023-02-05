26d12b5f889c23cc8443b51f5f700e1620785e95
Ammar Lakda <ammarlakdawala91@gmail.com>
2023-02-04 21:59:44 -0500

    chunks and summarizes

--- backend/summarize.py
+++ backend/summarize.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-from nltk import sent_tokenize
+from nltk import sent_tokenize, word_tokenize
 import nltk
 nltk.download('punkt')
 
@@ -9,28 +9,51 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 def summarize(Article):
   summary = summarizer(Article, max_length=350, min_length=200, do_sample=False)
   return summary
+
 def split_article(Article):
   #Maximum sequence length 
-  K = 950
+  K = 800
   #Total number of tokens in Document
-  N = len(sent_tokenize(Article))
-  sentences = sent_tokenize(Article)
+  N = len(word_tokenize(Article))
+  words = word_tokenize(Article)
   #Let I be the number of sequences of K tokens or less in Document
-  I = (N/K)
+  paddingSize = 50
+  I = (N//K)
   i =0
-  threshold = 15
-  chunks = [""] * (N//threshold + 1)
-  while i < N: 
-    if len(sentences[i]) > K:
-      sentences[i] = summarize(sentences[i])[0]["summary_text"]
-      
-    chunks[i//threshold] += " " + sentences[i]
-    i += 1
+  chunks = [""] * (I + 1)
+  print(N)
+  print(I)
+  while i < N:
+    # Padding start
+    if i != 0:
+      if i % K == 0:
+        counter = paddingSize
+        while counter > 0:
+          chunks[i//K] += words[i-counter] + " "
+          counter = counter - 1
+
+
+
+    chunks[i//K] += " " + words[i]
     
+    # Padding end
+    if i % K == K-1:
+        counter = 1
+        while counter <= paddingSize:
+          chunks[i//K] += words[i+counter] + " "
+          counter = counter + 1
+
+    i += 1
   master_string = ""
 
+  print(len(chunks))
   for i in range(len(chunks)):
+    
     temp = summarize(chunks[i])[0]["summary_text"]
+
     master_string += temp
+
+    print(temp)
   
+  print(master_string)
   return master_string
