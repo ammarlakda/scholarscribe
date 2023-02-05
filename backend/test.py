5603455382b9fcd128be72e05d30d2de48bdc14e
Ammar Lakda <ammarlakdawala91@gmail.com>
2023-02-04 21:45:57 -0500

    bullet points done

--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,8 @@
-from speech_text import speech_text
-
-print(speech_text("audio1.mp4"))
+i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+chunk_size=5
+overlap=2
+new_list=[]
+j=0
+while j < len(i):
+    j=j+chunk_size+overlap
+    new_list.append(j)
